85047342f3c14bd29a165fd0f5ed4d025a68fd0c
Jonathan Coe <jon@jbcoe.net>
2015-02-28 16:02:54 +0000

    extended filters to handle object types and c types

--- django_apps/templatetags/c_filter.py
+++ django_apps/templatetags/c_filter.py
@@ -2,18 +2,16 @@ from django import template
 import re
 
 register = template.Library()
+
 match_pointer_to_type = re.compile("const [A-Za-z0-9]+\s*\*$")
 
 @register.filter
 def to_c(s):
-  if s=='int':
-    return s
-  if s=='double':
-    return s
-  if s=='const char *':
+  if s=='int' or s=='bool' or s=='double' or s=='const char *':
     return s
   if re.match(match_pointer_to_type,s):
     return 'const void *'
   else:
-    raise Exception('Type {} has no known c equivalent'.format(s))
+    error = 'Type {} has no known c equivalent'.format(s)
+    raise Exception(error)
 
--- django_apps/templatetags/ctypes_filter.py
+++ django_apps/templatetags/ctypes_filter.py
@@ -1,8 +1,14 @@
 from django import template
+import re
+
 register = template.Library()
+
+match_pointer_to_type = re.compile("const [A-Za-z0-9]+\s*\*$")
 
 @register.filter
 def to_ctype(s):
+  if s=='bool':
+    return 'bool'
   if s=='int':
     return 'c_int'
   if s=='void*':
@@ -11,6 +17,9 @@ def to_ctype(s):
     return 'c_double'
   if s=='const char *':
     return 'c_char_p'
+  if re.match(match_pointer_to_type,s):
+    return 'c_object_p'
   else:
-    raise Exception('Type {} has no known ctypes equivalent'.format(s))
+    error = 'Type {} has no known ctypes equivalent'.format(s)
+    raise Exception(error)
 
