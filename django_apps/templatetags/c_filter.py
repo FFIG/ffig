d62a77174ce9d5692d6819ebe7aabf5f827801f8
Jonathan Coe <jon@jbcoe.net>
2015-03-17 20:56:16 +0000

    Improved handling of char* return type for c

--- django_apps/templatetags/c_filter.py
+++ django_apps/templatetags/c_filter.py
@@ -3,12 +3,15 @@ import re
 
 register = template.Library()
 
+match_pointer_to_char = re.compile("const char\s*\*$")
 match_pointer_to_type = re.compile("const [A-Za-z0-9]+\s*\*$")
 
 @register.filter
 def to_c(s):
-  if s=='void' or s=='int' or s=='bool' or s=='double' or s=='const char *':
+  if s=='void' or s=='int' or s=='bool' or s=='double':
     return s
+  if re.match(match_pointer_to_char,s):
+    return 'const char*'
   if re.match(match_pointer_to_type,s):
     return 'const void *'
   else:
