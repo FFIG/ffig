7ec4b589f9bfbb06f39bbee35ad19291ad81dd35
Jonathan Coe <jon@jbcoe.net>
2015-04-13 20:27:05 +0100

    tests for circle radius and area

---
+++ tests/test_python_bindings.py
@@ -0,0 +1,36 @@
+import unittest
+import sys
+import os
+import math
+
+testdir = os.path.dirname(__file__)
+moduledir = '../'
+modulepath = os.path.abspath(os.path.join(testdir, moduledir))
+sys.path.insert(0, modulepath)
+
+import Shape
+Shape.Config.set_library_path(modulepath)
+
+class TestPythonBindings(unittest.TestCase):
+  def test_Shape_Circle_has_expected_area(self):
+    radius = 2.0
+    area = radius * radius * math.pi
+
+    c = Shape.Circle(radius)
+    
+    self.assertAlmostEqual(area, c.area())
+
+  def test_Shape_Circle_has_expected_perimeter(self):
+    radius = 2.0
+    perimeter = 2.0 * math.pi * radius
+
+    c = Shape.Circle(radius)
+    
+    self.assertAlmostEqual(perimeter, c.perimeter())
+
+def main():
+  unittest.main()
+
+if __name__ == '__main__':
+  main()
+
