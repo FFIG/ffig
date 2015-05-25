13d6530ccbd5fdde17b97af43058cd4efb5a259b
Jonathan Coe <jon@jbcoe.net>
2015-05-25 18:44:15 +0100

    renamed template files

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 INPUT=${1:-Shape.h}
 OUTPUT=${INPUT%.h}
 
-./generate.py $INPUT h.tmpl ${OUTPUT}_c.h cpp.tmpl ${OUTPUT}_c.cpp py.tmpl ${OUTPUT}.py
+./generate.py $INPUT _c.h.tmpl ${OUTPUT}_c.h _c.cpp.tmpl ${OUTPUT}_c.cpp py.tmpl ${OUTPUT}.py
 
 clang++ -std=c++11 -shared -Wl, -o ${OUTPUT}_c.dylib -fPIC ${OUTPUT}_c.cpp
 
