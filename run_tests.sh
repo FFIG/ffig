ced6c46f8c2c4039fb430d3b627992d0712a961b
Jonathan Coe <jon@jbcoe.net>
2015-09-06 16:49:18 +0100

    running tests now runs code-gen

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,13 @@
-python -m unittest discover tests/
+#!/usr/bin/env bash
+
+HEADERS=Shape.h
+for HEADER in ${HEADERS}
+  do echo -n Generating bindings for ${HEADER}
+  ./build.sh ${HEADER} && echo " [OK]"
+done
+
+
+echo
+
+echo Running tests
+python -m unittest discover -v tests/
