0049646e49049a5a08ba054fa58f6209907e4dce
Jonathan Coe <jon@jbcoe.net>
2015-09-12 10:15:39 +0100

    run now uses a random radius

--- sandbox/main.m
+++ sandbox/main.m
@@ -2,8 +2,9 @@
 #import "Circle.h"
 
 int main(int argc, const char * argv[]) {
-  double r = 4.0;
-  Circle* circle = [[Circle alloc] initWithRadius:4.0];
-  printf("Circle with radius %f has perimeter %f and area %f\n", r, [circle perimeter], [circle area]);
+  double r = argc > 1 ? atoi(argv[1]) : 4.0;
+
+  Circle* circle = [[Circle alloc] initWithRadius:r];
+  printf("Circle with radius %f has perimeter %.2f and area %.2f\n", r, [circle perimeter], [circle area]);
   return 0;
 }
