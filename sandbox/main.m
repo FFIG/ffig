a04fa58a773ed9e1c534ddc20f6eb2f7bd940403
Jonathan Coe <jon@jbcoe.net>
2015-09-12 10:39:11 +0100

    radius printed out to 0dp

--- sandbox/main.m
+++ sandbox/main.m
@@ -5,7 +5,7 @@ int main(int argc, const char * argv[]) {
   double r = argc > 1 ? atoi(argv[1]) : 4.0;
 
   Circle* circle = [[Circle alloc] initWithRadius:r];
-  printf("%s with radius %f has perimeter %.2f and area %.2f\n",
+  printf("%s with radius %.0f has perimeter %.2f and area %.2f\n",
          [[circle name] UTF8String], r, [circle perimeter], [circle area]);
   return 0;
 }
