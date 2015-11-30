70c2df7ff75b0c759b4a52c53da041d67af4ca6b
Jonathan Coe <jon@jbcoe.net>
2015-11-30 23:05:42 +0000

    working prototype - needs tests

---
+++ sandbox/Tree.cpp
@@ -0,0 +1,105 @@
+#include <memory>
+#include <random>
+#include <iostream>
+
+static std::mt19937 mt;
+static std::uniform_int_distribution<int> d(1,10);
+static auto gen = []{return d(mt);};
+
+class Tree
+{
+  int data_;
+  std::shared_ptr<Tree> left_;
+  std::shared_ptr<Tree> right_;
+
+  public:
+
+  Tree(int levels=0)
+  {
+    data_ = gen();
+    std::cout << "Building a tree with data " << data_ << " and " << levels << " more levels" << std::endl;
+    if ( levels <= 0 ) return;
+    left_ = std::make_shared<Tree>(levels-1);
+    right_ = std::make_shared<Tree>(levels-1);
+  }
+
+  const Tree* left_subtree() const
+  {
+    return left_.get();
+  }
+
+  const Tree* right_subtree() const
+  {
+    return right_.get();
+  }
+
+  int data() const
+  {
+    return data_;
+  }
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+  struct Tree_block
+  {
+    std::shared_ptr<const Tree> object_;
+    Tree_block(const Tree_block&) = delete;
+
+    Tree_block() = default;
+
+    template<typename T, typename ...Ts>
+      static Tree_block* create(Ts&&...ts)
+      {
+        auto block = std::make_unique<Tree_block>();
+        block->object_ = std::make_shared<T>(std::forward<Ts>(ts)...);
+        return block.release();
+      }
+
+    template<typename ParentBlock>
+      static Tree_block* create_subobject(const ParentBlock* parent, const Tree* object)
+      {
+        auto block = std::make_unique<Tree_block>();
+        //use aliasing constructor of shared_ptr to keep parent alive
+        block->object_ = std::shared_ptr<const Tree>(parent->object_, object);
+        return block.release();
+      }
+  };
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+extern "C" {
+
+  const void* Tree_create(int n)
+  {
+    return Tree_block::create<Tree>(n);
+  }
+
+  void Tree_dispose(const void* tree)
+  {
+    delete reinterpret_cast<const Tree_block*>(tree);
+  }
+
+  const void* Tree_left_subtree(const void* tree)
+  {
+    auto block = reinterpret_cast<const Tree_block*>(tree);
+    return Tree_block::create_subobject(block, block->object_->left_subtree());
+  }
+
+  const void* Tree_right_subtree(const void* tree)
+  {
+    auto block = reinterpret_cast<const Tree_block*>(tree);
+    return Tree_block::create_subobject(block, block->object_->right_subtree());
+  }
+
+  int Tree_data(const void* tree)
+  {
+    auto block = reinterpret_cast<const Tree_block*>(tree);
+    return block->object_->data();
+  }
+}
---
+++ sandbox/Tree.py
@@ -0,0 +1,129 @@
+from ctypes import *
+c_object_p = POINTER(c_void_p)
+
+class Tree:
+  
+  def right(self):   
+    t = Tree(None)
+    t.ptr = conf.lib.Tree_right_subtree(self);
+    return t
+    
+  def left(self):   
+    t = Tree(None)
+    t.ptr = conf.lib.Tree_left_subtree(self);
+    return t
+
+  def data(self):   
+    return conf.lib.Tree_data(self);
+    
+  def from_param(self):
+    return self.ptr
+
+  def __del__(self):
+    conf.lib.Tree_dispose(self)
+
+  def __init__(self, n=None):
+      if not n: 
+          return
+      self.ptr = conf.lib.Tree_create(n)
+
+
+methodList = [
+  
+  ("Tree_dispose",
+  [Tree],
+  None), 
+  
+  ("Tree_create",
+  [c_int],
+  c_object_p), 
+  
+  ("Tree_right_subtree",
+  [Tree],
+  c_object_p),
+  
+  ("Tree_left_subtree",
+  [Tree],
+  c_object_p),
+  
+  ("Tree_data",
+  [Tree],
+  c_int)
+]
+
+# library loading and method registrations
+# based on clang python bindings approach
+
+def register_method(lib, item):
+  func = getattr(lib, item[0])
+
+  if len(item) >= 2:
+    func.argtypes = item[1]
+
+  if len(item) >= 3:
+    func.restype = item[2]
+
+
+class CachedProperty(object):
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+        try:
+            self.__doc__ = wrapped.__doc__
+        except:
+            pass
+
+    def __get__(self, instance, instance_type=None):
+        if instance is None:
+            return self
+
+        value = self.wrapped(instance)
+        setattr(instance, self.wrapped.__name__, value)
+
+        return value
+
+class Config:
+    library_path = None
+    loaded = False
+
+    @staticmethod
+    def set_library_path(path):
+        if Config.loaded:
+            raise Exception("library path is already set.")
+        Config.library_path = path
+
+    @CachedProperty
+    def lib(self):
+        lib = self._get_library()
+        map(lambda x:register_method(lib,x), methodList)
+        Config.loaded = True
+        return lib
+
+    def _get_filename(self):
+        import platform
+        name = platform.system()
+
+        if name == 'Darwin':
+            file = 'libTree_c.dylib'
+        elif name == 'Windows':
+            file = 'Tree_c.dll'
+        else:
+            file = 'libTree_c.so'
+
+        if Config.library_path:
+            file = Config.library_path + '/' + file
+
+        return file
+
+    def _get_library(self):
+        try:
+            library = cdll.LoadLibrary(self._get_filename())
+        except OSError as e:
+            msg = str(e) + ". To provide a path to Tree dylib use Config.set_library_path()"
+            raise Exception(msg)
+
+        return library  
+
+conf = Config()
+
+
