97ef11ed8bb6b0662fd9d293eb631c5d1874eeda
Jonathan Coe <jon@jbcoe.net>
2015-12-02 00:27:10 +0000

    Add tree demo to input - needed for subobjects

---
+++ input/Tree.h
@@ -0,0 +1,40 @@
+#include <memory>
+#include <random>
+
+static std::mt19937 mt;
+static std::uniform_int_distribution<int> d(1,10);
+static auto gen = []{return d(mt);};
+
+class Tree
+{
+  int data_;
+  std::shared_ptr<Tree> left_;
+  std::shared_ptr<Tree> right_;
+
+  public:
+
+  Tree(int levels=0)
+  {
+    data_ = gen();
+    if ( levels <= 0 ) return;
+    left_ = std::make_shared<Tree>(levels-1);
+    right_ = std::make_shared<Tree>(levels-1);
+  }
+
+  const Tree* left_subtree() const
+  {
+    return left_.get();
+  }
+
+  const Tree* right_subtree() const
+  {
+    return right_.get();
+  }
+
+  int data() const
+  {
+    return data_;
+  }
+
+};
+
