7bd3686cfb38b309356ba4d9bd6a7df4d1789b08
Jonathan Coe <jon@jbcoe.net>
2016-05-20 08:18:09 +0100

    strip hidden symbols

--- _build.sh
+++ _build.sh
@@ -7,5 +7,6 @@ cp $INPUT output/
 pushd output > /dev/null
 INPUT_FILE=$(basename $INPUT)
 OUTPUT=${INPUT_FILE%.h}
-clang++-3.6 -std=c++14 -shared -Wl, -o lib${OUTPUT}_c.dylib -fPIC ${OUTPUT}_c.cpp || exit 1
+clang++-3.7 -fvisibility=hidden -g -std=c++14 -shared -Wl, -o lib${OUTPUT}_c.dylib -fPIC ${OUTPUT}_c.cpp || exit 1
+strip -x lib${OUTPUT}_c.dylib
 popd > /dev/null
