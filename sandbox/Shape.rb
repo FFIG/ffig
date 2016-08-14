d58a4e447094ecec862edd2646faa2ca33e9ac81
Jonathan B. Coe <jbcoe@me.com>
2016-08-14 22:38:01 +0100

    MVP - time for code-gen

--- sandbox/Shape.rb
+++ sandbox/Shape.rb
@@ -1,33 +1,6 @@
 require 'ffi'
 
-class Foo
-  attr_reader :bar
-  def initialize(n)
-    @bar = n
-    ObjectSpace.define_finalizer( self, self.class.finalize(bar) )
-  end
-
-  def self.finalize(bar)
-    proc { puts "DESTROY OBJECT #{bar}" }
-  end
-
-end
-
-
-f=Foo.new(101)
-puts "Foo.bar is #{f.bar} now"
-f=nil
-
-# Force ruby to start the Garbage Collector
-# In a real program you don't have to do this
-# ruby will run the GC automatically.
-GC.start
-sleep 1 # make sure you will see the message
-        # before ruby quits
-puts "done"
-
-
-module Shape
+module Shape_c
   extend FFI::Library
   ffi_lib 'Shape_c'
   attach_function :Shape_error, [], :string
@@ -39,21 +12,50 @@ module Shape
   attach_function :Shape_dispose, [:pointer], :void
 end
 
-objptr = FFI::MemoryPointer.new :pointer
-c = Shape.Shape_Circle_create(10, objptr)
-objptr = objptr.get_pointer(0)
+class Circle
+  attr_reader :ptr
+  def initialize(radius)
+    objptr = FFI::MemoryPointer.new :pointer
+    rc = Shape_c.Shape_Circle_create(10, objptr)
+    if rc != 0
+      msg = Shape_c.Shape_error
+      Shape_c.Shape_clear_error()
+      raise Exception.new(msg)
+    end
+    @ptr = objptr.get_pointer(0)
 
-dptr = FFI::MemoryPointer.new :double
-Shape.Shape_perimeter(objptr, dptr)
-puts "Perimeter is #{dptr.get_double(0)}"
+    ObjectSpace.define_finalizer( self, self.class.finalize(ptr) )
+  end
 
-dptr = FFI::MemoryPointer.new :double
-Shape.Shape_area(objptr, dptr)
-puts "Area is #{dptr.get_double(0)}"
+  def area()
+    dptr = FFI::MemoryPointer.new :double
+    Shape_c.Shape_area(ptr, dptr)
+    return dptr.get_double(0)
+  end
+  
+  def perimeter()
+    dptr = FFI::MemoryPointer.new :double
+    Shape_c.Shape_perimeter(ptr, dptr)
+    return dptr.get_double(0)
+  end
 
-Shape.Shape_dispose(objptr)
+  def self.finalize(ptr)
+    proc { puts "disposing of Shape at #{ptr}"
+           Shape_c.Shape_dispose(ptr) }
+  end
+end
 
-Shape.Shape_Circle_create(-10, objptr)
-puts "Error is #{Shape.Shape_error}"
-Shape.Shape_clear_error()
+r = 8
+c = Circle.new(8)
+puts "Radius = #{r}"
+puts "Perimeter = #{c.perimeter}"
+puts "Area = #{c.area}"
 
+# Force ruby to start the Garbage Collector
+# In a real program you don't have to do this
+# ruby will run the GC automatically.
+c = nil
+
+GC.start
+sleep 1 # make sure you will see the message
+        # before ruby quits
