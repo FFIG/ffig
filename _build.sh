74af6e3f55aca813a842b380f18c690bd7db5a19
Jonathan B. Coe <jbcoe@me.com>
2016-08-20 10:28:22 +0100

    use libc++ when building c-api dll

--- _build.sh
+++ _build.sh
@@ -7,6 +7,6 @@ cp $INPUT output/
 pushd output > /dev/null
 INPUT_FILE=$(basename $INPUT)
 OUTPUT=${INPUT_FILE%.h}
-clang++-3.8 -fvisibility=hidden -g -std=c++14 -shared -Wl, -o lib${OUTPUT}_c.dylib -fPIC ${OUTPUT}_c.cpp || exit 1
+clang++-3.8 -fvisibility=hidden -g -std=c++14 -stdlib=libc++ -shared -Wl, -o lib${OUTPUT}_c.dylib -fPIC ${OUTPUT}_c.cpp || exit 1
 strip -x lib${OUTPUT}_c.dylib
 popd > /dev/null
