9e24a7e10b5c6ebdca8d9bd9c4035ccb9fe4f37d
Jonathan B. Coe <jbcoe@me.com>
2016-08-22 13:18:21 +0100

    tests for hand-written ruby bindings

--- sandbox/Shape.rb
+++ sandbox/Shape.rb
@@ -6,6 +6,8 @@ module Shape_c
   attach_function :Shape_error, [], :string
   attach_function :Shape_clear_error, [], :void
   attach_function :Shape_Circle_create, [:double, :pointer], :int
+  attach_function :Shape_Pentagon_create, [:double, :pointer], :int
+  attach_function :Shape_Square_create, [:double, :pointer], :int
   attach_function :Shape_area, [:pointer, :pointer], :int 
   attach_function :Shape_perimeter, [:pointer, :pointer], :int 
   attach_function :Shape_name, [:pointer, :pointer], :int 
@@ -28,6 +30,13 @@ class Shape
     dptr = FFI::MemoryPointer.new :double
     Shape_c.Shape_perimeter(@ptr, dptr)
     dptr.get_double(0)
+  end
+
+  def name()
+    dptr = FFI::MemoryPointer.new(:pointer, 1)
+    Shape_c.Shape_name(@ptr, dptr)
+    strPtr = dptr.read_pointer()
+    return strPtr.null? ? nil : strPtr.read_string()
   end
 
   def self.finalize(ptr)
@@ -77,20 +86,20 @@ class Square < Shape
   end
 end
 
-r = 8
-c = Circle.new(8)
-puts "Radius = #{r}"
-puts "Perimeter = #{c.perimeter}"
-puts "Area = #{c.area}"
-
-# Force ruby to start the Garbage Collector
-# In a real program you don't have to do this
-# ruby will run the GC automatically.
-c = nil
-
-c = Circle.new(-10)
-
-GC.start
-sleep 1 # make sure you will see the message
-        # before ruby quits
-puts "Done"
+# r = 8
+# c = Circle.new(8)
+# puts "Radius = #{r}"
+# puts "Perimeter = #{c.perimeter}"
+# puts "Area = #{c.area}"
+# 
+# # Force ruby to start the Garbage Collector
+# # In a real program you don't have to do this
+# # ruby will run the GC automatically.
+# c = nil
+# 
+# c = Circle.new(-10)
+# 
+# GC.start
+# sleep 1 # make sure you will see the message
+#         # before ruby quits
+# puts "Done"
