23848b2e3abcebc03e13438e486d0a1c055dcfd0
Jonathan B. Coe <jbcoe@me.com>
2016-11-10 10:08:35 -0800

    remove references to clang 3.8

--- _build.sh
+++ _build.sh
@@ -5,10 +5,7 @@ DYLIB_EXT="so"
 #FIXME: Find a more idiomatic way to detect the OS.
 if [[ $OSTYPE =~ .*darwin.* ]] ; then DYLIB_EXT="dylib"; fi
 
-# Use clang++-3.8 explicitly, if available. Fall back on available clang++
-# otherwise.
-CLANG=clang++-3.8
-which ${CLANG} > /dev/null || CLANG=clang++
+CLANG=clang++
 
 ./generate.py $INPUT templates output || exit 1
 cp $INPUT output/
