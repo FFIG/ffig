185669a41c73f35b65ddca0ce85f9580baeb4513
Jonathan B. Coe <jbcoe@me.com>
2017-02-06 14:41:55 +0000

    changes to build.py for Win64 build

--- scripts/build.py
+++ scripts/build.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+import platform
 import subprocess
 
 
@@ -8,44 +9,50 @@ def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-t", help="run tests", action="store_true", dest="run_tests")
+        '-t', help='run tests', action='store_true', dest='run_tests')
     parser.add_argument(
-        "-v", help="verbose", action="store_true", dest="verbose")
+        '-v', help='verbose', action='store_true', dest='verbose')
     parser.add_argument(
-        "-o",
-        help="output dir (relative to source dir)",
-        default="build",
-        dest="out_dir")
+        '-o',
+        help='output dir (relative to source dir)',
+        default='build',
+        dest='out_dir')
     parser.add_argument(
-        "-c",
-        help="config (Debug or Release)",
-        default="Debug",
-        dest="config")
+        '-c',
+        help='config (Debug or Release)',
+        default='Debug',
+        dest='config')
     parser.add_argument(
-        "--python-path",
-        help="path to python executable ie '/usr/local/bin/python3'",
-        dest="python_path")
+        '--python-path',
+        help='path to python executable ie "/usr/local/bin/python3"',
+        dest='python_path')
     args = parser.parse_args()
-
+    args.platform = platform.system()
+    
     src_dir = os.path.dirname(os.path.dirname(__file__))
 
-    cmake_invocation = "cmake . -B{} -DCMAKE_BUILD_TYPE={}".format(args.out_dir, args.config)    
-    if args.verbose:
-        cmake_invocation = cmake_invocation + " -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON"
-    if args.python_path:
-        cmake_invocation = cmake_invocation + " -DPYTHON_EXECUTABLE={}".format(args.python_path)
+    cmake_invocation = ['cmake', '.','-B{}'.format(args.out_dir)]
+    if args.platform == 'Windows':
+        cmake_invocation.append('-G"Visual Studio 14 2015 Win64"')
+    else:
+        cmake_invocation.append('-DCMAKE_BUILD_TYPE={}'.format(args.config))
 
-    subprocess.check_call(cmake_invocation.split(), cwd=src_dir)
+    if args.verbose:
+        cmake_invocation.append('-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON')
+    if args.python_path:
+        cmake_invocation.append('-DPYTHON_EXECUTABLE={}'.format(args.python_path))
+
+    subprocess.check_call(cmake_invocation, cwd=src_dir)
     subprocess.check_call(
-        "cmake --build ./{}".format(args.out_dir).split(), cwd=src_dir)
+        'cmake --build ./{}'.format(args.out_dir).split(), cwd=src_dir)
 
     if args.run_tests:
         rc = subprocess.call(
-            "ctest . --output-on-failure -c {}".format(args.config).split(),
+            'ctest . --output-on-failure -C {}'.format(args.config).split(),
             cwd=os.path.join(src_dir, args.out_dir))
         if rc != 0:
             sys.exit(1)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
